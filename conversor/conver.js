c7f6eca3a1547184e5c257af9a4d883e8a1f8ed6
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-05-14 21:23:24 -0400

    tualizando

--- conversor/conver.js
+++ conversor/conver.js
@@ -5,7 +5,7 @@ const valor2 = document.getElementById("moneyuea")
 const selecionando = document.getElementById("outros")
 const pais = document.getElementById('pais')
 
-const convertendo = conversao =>{
+const convertendo = ()  =>{
 
     if(selecionando.value == "001"){
 
@@ -50,14 +50,52 @@ const convertendo = conversao =>{
         valor2.innerHTML= ` ₿ ${conversaofinal.toFixed(2)}`
     
 }
-return conversao }
+return  }
 
-const selecionandomoeda = ()=>{}
+const selecionandomoeda = (e)=>{
+    const teste = e.srcElement
+   const bandeira = teste.value
+
+   if(bandeira=="001"){
+
+    const foto = document.getElementById("foto02")
+    foto.src="./germany.png"
+    pais.innerHTML="GERMANY" 
+    const num1 = Number(valor.value)
+    
+    const conversaofinal= num1 /5.36
+    valor2.style.color= 'blue'
+    valor2.innerHTML= `€ ${conversaofinal.toFixed(2)}`
+
+}else if(bandeira=="002"){
+    const foto = document.getElementById("foto02")
+    foto.src="./eua.png"
+    pais.innerHTML="UEA"     
+    const num1 = Number(valor.value)
+    
+    const conversaofinal= num1 /4.91
+    valor2.style.color= 'blue'
+    valor2.innerHTML= `U$ ${conversaofinal.toFixed(2)}`
+
+
+}else{
+    const foto = document.getElementById("foto02")
+    foto.src="./bitcoin.png"
+    pais.innerHTML="bitcoin"
+    const num1 = Number(valor.value)
+    
+    const conversaofinal= num1 /178.727
+    valor2.style.color= 'blue'
+    valor2.innerHTML= ` ₿ ${conversaofinal.toFixed(2)}`
+
+}
+    
+}
 
 const converter = document.getElementById("convert")
 .addEventListener('click', convertendo)
 
-const moedas = document.getElementById("outros")
-.addEventListener('change', selecionandomoeda)
 
+const teste = document.getElementById("outros")
+.addEventListener('click', selecionandomoeda)
 
